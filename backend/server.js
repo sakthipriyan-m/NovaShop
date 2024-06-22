3873dee0a57433b01ac91925b3e5ed810db714a2
sakthipriyan-m <sakthipriyan.m99@gmail.com>
2024-06-22 16:27:35 +0530

    Second commit
    FrontEnd -Created Header, Footer, Home Page, Product page.
    backEnd: Connected with DB

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+import products from './data/products.js';
+connectDB();
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send('API is running....');
+});
+app.get('/api/products', (req, res)=> {
+    res.json(products);
+});
+app.get('/api/products/:id', (req, res)=> {
+    const product = products.find((p)=> p._id === req.params.id);
+    res.json(product);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <h1>Welcome to NovaShop</h1>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+        <Footer />
+      </main>
+    </>
+  );
+};
 
-export default App
+export default App;
