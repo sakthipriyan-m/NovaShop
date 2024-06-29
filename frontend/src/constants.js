93f42e8e4344f181ff8026c92cf640951c472d71
sakthipriyan-m <sakthipriyan.m99@gmail.com>
2024-06-30 00:12:31 +0530

    Redux store

---
+++ frontend/src/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+export const USERS_URL = '/api/users';
+export const PRODUCT_URL = '/api/products';
+export const ORDER_URL = '/api/config/paypal';
+export const PAYPAL_URL = '/api/products';
