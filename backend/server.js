20c8e7e308e50c3a8df2cfb574436c6009b4d636
sakthipriyan-m <sakthipriyan.m99@gmail.com>
2024-07-02 22:33:47 +0530

    Auth Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 connectDB();
 
 const PORT = process.env.PORT || 5050;
@@ -12,14 +13,17 @@ const app = express();
 
 //Body Parser middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res)=> {
-    res.send('API is running....');
+//cookie parser middleware
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("API is running....");
 });
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
