68a0bbce289b6508b94c982a882c50f89c812d22
sakthipriyan-m <sakthipriyan.m99@gmail.com>
2024-07-07 10:52:15 +0530

    FrontEnd Login Slice

---
+++ frontend/src/slices/loginSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+};
+
+const loginSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    },
+  },
+});
+
+export const { setCredentials } = loginSlice.actions;
+
+export default loginSlice.reducer;
