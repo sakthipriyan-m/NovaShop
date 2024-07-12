62730acb58c10c87de9e3cc91e16192511fa7f76
sakthipriyan-m <sakthipriyan.m99@gmail.com>
2024-07-12 17:44:11 +0530

    Private Route : Restrict the Unauthenticated users to access the Authenticated Pages
    Redirects unauthenticated users to the login page if they attempt to access restricted pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,7 @@ import CartPage from './pages/CartPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ShippingPage from './pages/ShippingPage';
+import PrivateRoute from './components/PrivateRoute';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,7 +23,9 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path='' element={<PrivateRoute />}>
       <Route path="/shipping" element={<ShippingPage />} />
+      </Route>
     </Route>
   )
 );
