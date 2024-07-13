736d4b745eac8f3a4868d5b6ef47fbe15577d1fa
sakthipriyan-m <sakthipriyan.m99@gmail.com>
2024-07-13 17:15:58 +0530

    Shipping Charge Update

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -6,8 +6,8 @@ export const updateCart =(state) => {
     //Calculate items Price
     state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.listPrice * item.quantity, 0));
 
-    //Calculate Shipping price (if the order id over 500Rs then free shipping, else 49rs shipping).
-    state.shippingPrice = addDecimals(state.itemsPrice > 500 ? 0 : 50);
+    //Calculate Shipping price (if the order is over 1000Rs then free shipping, else 50rs shipping).
+    state.shippingPrice = addDecimals(state.itemsPrice > 1000 ? 0 : 50);
 
     //Calculate tax price (18% tax)
    state.taxPrice = addDecimals(Number((0.18 * state.itemsPrice).toFixed(2)));
