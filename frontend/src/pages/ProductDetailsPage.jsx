c9717c9aacfaa4439787205121ca8619ead4d9cd
sakthipriyan-m <sakthipriyan.m99@gmail.com>
2024-07-27 22:31:05 +0530

    Update Loader
    - Created separate folder for Loader
     -Added Custom CSS file

--- frontend/src/pages/ProductDetailsPage.jsx
+++ frontend/src/pages/ProductDetailsPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import {Row, Col, Image, ListGroup, Card, Button, Alert } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import Rating from "../components/Rating";
-import Loader from "../components/Loader";
+import Loader from "../components/Loader/Loader";
 import Message from "../components/Message";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 import { addToCart } from "../slices/cartSlice";
